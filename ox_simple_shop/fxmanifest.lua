6461165401e486fd58fb10b2ade57548c81c1671
KillerStreamer <87175312+KillerStreamer@users.noreply.github.com>
2026-01-29 18:40:57 +0100

    Add ox_simple_shop billing resource

---
+++ ox_simple_shop/fxmanifest.lua
@@ -0,0 +1,20 @@
+-- fxmanifest.lua content here
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Your Name'
+description 'ox_simple_shop billing resource'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+shared_script 'config.lua'
+
+files {
+    'html/*'
+}
+
+loadscreen 'html/index.html'
+
+ui_page 'html/index.html'
